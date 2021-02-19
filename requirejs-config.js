1227bab39cbdf32b7400635e89ca70da0bccb245
TrongHoa <34540606+Nagaruu@users.noreply.github.com>
2021-02-19 16:11:56 +0700

    abc

---
+++ requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    paths: {            
+            fontawesome: "js/fontawesome",
+            bootstrapjs: "js/bootstrap.min",
+            'owlcarousel': "js/owl.carousel"
+        },
+    shim: {
+        'fontawesome': {
+            deps: ['jquery']
+        },
+        'owlcarousel': {
+            deps: ['jquery']
+        }
+    },
+};
